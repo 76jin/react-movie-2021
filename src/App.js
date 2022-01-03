fce02bf32823af160d977f2c7c152e558f5ac838
ranian <ranian129@gmail.com>
2022-01-03 16:16:44 +0900

    영화 데이터 state에 저장

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import React from "react";
+import axios from "axios";
+import factoryWithThrowingShims from "prop-types/factoryWithThrowingShims";
+// import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome ReactJS</h1>
-      <Button text={"Continue"} />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    // console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    // TODO: 영화 데이터 로딩
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "TODO: 영화 데이터 출력"}</div>;
+  }
 }
 
 export default App;
