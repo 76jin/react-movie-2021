af077c22e3066bddf8e58815eb311f683e72462f
ranian <ranian129@gmail.com>
2022-01-03 17:03:42 +0900

    장르 props 추가

--- src/Movie.js
+++ src/Movie.js
@@ -2,13 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./Movie.module.css";
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
   return (
-    <div className="movie_data">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <h3 className="movie__title">{title}</h3>
-      <h5 className="movie_year">{year}</h5>
-      <p className="movie_summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <p className="movie_summary">{summary}</p>
+        <ul className="movie_genres">
+          {genres.map((genre, index) => {
+            return (
+              <li key={index} className="movie_genre">
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
@@ -18,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
