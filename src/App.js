6c5752fa16a3e1e9532e83d8a72e0f11bbffde5e
ranian <ranian129@gmail.com>
2022-01-04 17:42:57 +0900

    라우터 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
 import Home from "./routes/Home";
 import "./App.css";
-// import styles from "./App.module.css"
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
