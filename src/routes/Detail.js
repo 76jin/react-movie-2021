d0d525092036034755fe215e82265d50863e5c51
ranian <ranian129@gmail.com>
2022-01-05 23:14:20 +0900

    영화 상세 정보 리다이렉트 추가

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,41 @@
 import React from "react";
+import "./Detail.css";
 
-function Detail(props) {
-  console.log(props);
-  return <span>Detail page</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      const { year, title, summary, poster, genres } = location.state;
+      return (
+        <div className="movie detail">
+          <img src={poster} alt={title} title={title} />
+          <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">
+              {genres.map((genre, index) => {
+                return (
+                  <li key={index} className="genres__genre">
+                    {genre}
+                  </li>
+                );
+              })}
+            </ul>
+            <p className="movie__summary">{summary}</p>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
